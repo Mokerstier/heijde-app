ce6f72b6915567af31ee9a7185227461ecd8c016
MokerStier <wouter.van.der.heijde@outlook.com>
2024-12-14 16:08:44 +0100

    Added hamburger toggle

--- components/layout/MainNavigation.vue
+++ components/layout/MainNavigation.vue
@@ -6,38 +6,63 @@
                     <NuxtImg class="h-4 w-4" src="W-logo-light.svg" /> Wouter
                 </NuxtLink>
             </li>
-            <li class="ml-auto">
-                <NuxtLink active-class="text-white" class="text-gray underline-animation" to="/"
+            <li class="ml-auto hidden md:block">
+                <NuxtLink active-class="text-white" class="underline-animation text-gray" to="/"
                     ><span class="text-primary">#</span>home</NuxtLink
                 >
             </li>
-            <li>
+            <li class="hidden md:block">
                 <NuxtLink
                     active-class="text-white"
-                    class="text-gray underline-animation"
+                    class="underline-animation text-gray"
                     to="/projects"
                     ><span class="text-primary">#</span>projects</NuxtLink
                 >
             </li>
-            <li>
+            <li class="hidden md:block">
                 <NuxtLink
                     active-class="text-white"
-                    class="text-gray underline-animation"
+                    class="underline-animation text-gray"
                     to="/about"
                     ><span class="text-primary">#</span>about</NuxtLink
                 >
             </li>
-            <li>
+            <li class="hidden md:block">
                 <NuxtLink
                     active-class="text-white"
-                    class="text-gray underline-animation"
+                    class="underline-animation text-gray"
                     to="/contact"
                     ><span class="text-primary">#</span>contact</NuxtLink
                 >
             </li>
+            <li class="ml-auto block md:hidden">
+                <button
+                    @click="toggleMenu"
+                    :aria-label="menuIsOpen ? 'close the naviagtion menu' : 'open navigation menu'"
+                    class="flex h-full flex-col items-center justify-center gap-y-[5px]">
+                    <span
+                        class="button__menu button__menu--top h-[2px] w-6 bg-gray transition-all"
+                        :class="{ 'translate-y-[0.4rem] rotate-45': menuIsOpen }"></span>
+                    <span
+                        class="button__menu button__menu--mid ml-auto h-[2px] w-4 bg-gray transition-all"
+                        :class="{ 'opacity-0': menuIsOpen }"></span>
+                    <span
+                        class="button__menu button__menu--bot h-[2px] w-6 bg-gray transition-all"
+                        :class="{ '-translate-y-2 -rotate-45': menuIsOpen }"></span>
+                </button>
+            </li>
         </ul>
     </nav>
 </template>
+
+<script lang="ts" setup>
+const menuIsOpen = ref(false);
+
+const toggleMenu = () => {
+    menuIsOpen.value = !menuIsOpen.value;
+};
+</script>
+
 <style lang="scss">
 .underline-animation {
     @apply relative;
