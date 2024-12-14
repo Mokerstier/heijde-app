9adffc583a813a5cd02358f1acfc38b4a55fc66e
MokerStier <wouter.van.der.heijde@outlook.com>
2024-12-14 22:07:40 +0100

    Updates

---
+++ _nuxt/BwFAmzf5.js
@@ -0,0 +1,1 @@
+import{m as e,o as n,c as o}from"./BO5cnJbC.js";const s=e({__name:"index",setup(t){return(a,c)=>(n(),o("h1",null,"Blog"))}});export{s as default};
---
+++ _nuxt/oRsXRa1M.js
@@ -0,0 +1,1 @@
+import{m as n,o as a,c as o,t}from"./BO5cnJbC.js";const m=n({__name:"index",setup(r){return(e,s)=>(a(),o("h1",null,"Welcome back: "+t(e.user.name),1))}});export{m as default};
