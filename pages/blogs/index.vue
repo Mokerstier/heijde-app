af90b3dd5ce681b165d39ec5ffbfe8fc201af93e
MokerStier <wouter.van.der.heijde@outlook.com>
2024-12-15 14:55:17 +0100

    Update blogs

--- pages/blogs/index.vue
+++ pages/blogs/index.vue
@@ -3,14 +3,4 @@
         <Header prefix="/" title="blogs" sub-title="Under construction"></Header>
     </section>
 </template>
-<script lang="ts" setup>
-import { Octokit } from 'octokit';
-const { GITHUB_TOKEN } = useRuntimeConfig();
-const octokit = new Octokit({
-    auth: GITHUB_TOKEN,
-});
-
-const blogs = await octokit.request('GET /repos/MokerStier/weekly-nerd-1920/wiki/');
-
-console.log(blogs);
-</script>
+<script lang="ts" setup></script>
