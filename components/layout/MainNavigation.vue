f8fe2ae0357ebbce9a7e9f00432dd1cf1f2265f7
Wouter <wouter.van.der.heijde@outlook.com>
2024-12-16 22:26:16 +0100

    Added MobileNavigation

--- components/layout/MainNavigation.vue
+++ components/layout/MainNavigation.vue
@@ -1,47 +1,92 @@
 <template>
-    <nav class="container pb-2 pt-8">
-        <ul class="flex gap-x-8">
-            <li>
-                <NuxtLink class="flex items-center gap-x-4" to="/">
-                    <NuxtImg class="h-4 w-4" src="W-logo-light.svg" /> Wouter
-                </NuxtLink>
-            </li>
-            <li v-for="route in routes" class="hidden md:block [&:nth-child(2)]:ml-auto">
-                <Button
-                    :as-link="true"
-                    variant="secondary"
-                    active-class="text-white"
-                    class="underline-animation text-gray"
-                    :to="route.to">
-                    <span> <span class="text-primary">#</span>{{ route.label }} </span>
-                </Button>
-            </li>
-            <li class="ml-auto block md:hidden">
-                <button
-                    @click="toggleMenu"
-                    :aria-label="menuIsOpen ? 'close the naviagtion menu' : 'open navigation menu'"
-                    class="flex h-full flex-col items-center justify-center gap-y-[5px]">
-                    <span
-                        class="button__menu button__menu--top h-[2px] w-6 bg-gray transition-all"
-                        :class="{ 'translate-y-[0.4rem] rotate-45': menuIsOpen }"></span>
-                    <span
-                        class="button__menu button__menu--mid ml-auto h-[2px] w-4 bg-gray transition-all"
-                        :class="{ 'opacity-0': menuIsOpen }"></span>
-                    <span
-                        class="button__menu button__menu--bot h-[2px] w-6 bg-gray transition-all"
-                        :class="{ '-translate-y-2 -rotate-45': menuIsOpen }"></span>
-                </button>
-            </li>
-        </ul>
-    </nav>
+    <div
+        ref="mainNav"
+        class="relative border-b transition-colors"
+        :class="{
+            'border-b-gray/75 delay-500': menuIsOpen,
+            'border-b-slate delay-0': !menuIsOpen,
+        }">
+        <nav class="container pb-2 pt-4 lg:pt-8">
+            <ul class="flex gap-x-8">
+                <li>
+                    <NuxtLink class="flex items-center gap-x-3" to="/">
+                        <NuxtImg class="h-4 w-4" src="W-logo-light.svg" /> Wouter
+                    </NuxtLink>
+                </li>
+                <li v-for="route in routes" class="hidden md:block [&:nth-child(2)]:ml-auto">
+                    <Button
+                        :as-link="true"
+                        variant="secondary"
+                        active-class="text-white"
+                        class="underline-animation text-gray"
+                        :to="route.to">
+                        <span> <span class="text-primary">#</span>{{ route.label }} </span>
+                    </Button>
+                </li>
+                <li class="ml-auto block md:hidden">
+                    <button
+                        @click="toggleMenu"
+                        :aria-label="
+                            menuIsOpen ? 'close the naviagtion menu' : 'open navigation menu'
+                        "
+                        class="flex h-full flex-col items-center justify-center gap-y-[5px]">
+                        <span
+                            class="button__menu button__menu--top h-[2px] w-6 bg-white transition-all"
+                            :class="{ 'translate-y-[0.4rem] rotate-45': menuIsOpen }"></span>
+                        <span
+                            class="button__menu button__menu--mid ml-auto h-[2px] w-4 bg-white transition-all"
+                            :class="{ 'opacity-0': menuIsOpen }"></span>
+                        <span
+                            class="button__menu button__menu--bot h-[2px] w-6 bg-white transition-all"
+                            :class="{ '-translate-y-2 -rotate-45': menuIsOpen }"></span>
+                    </button>
+                </li>
+            </ul>
+            <MobileNavigation :menu-is-open="menuIsOpen" />
+        </nav>
+    </div>
 </template>
 
 <script lang="ts" setup>
-import { useNavigation } from '../composables/useNavigation';
+import { useRoute } from 'vue-router';
+const route = useRoute();
 
 const menuIsOpen = ref(false);
+const mainNav = ref<HTMLElement | null>(null);
 const routes = useNavigation();
 const toggleMenu = () => {
     menuIsOpen.value = !menuIsOpen.value;
 };
+
+watch(
+    () => route.fullPath,
+    () => {
+        menuIsOpen.value = false;
+    }
+);
+
+watch(
+    () => menuIsOpen.value,
+
+    (isOpen) => {
+        if (isOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'auto';
+        }
+    }
+);
+
+onMounted(() => {
+    document.documentElement.style.setProperty(
+        '--nav-height',
+        `${mainNav.value?.getBoundingClientRect().height ?? 48 + 1}px`
+    );
+    window.addEventListener('resize', () => {
+        document.documentElement.style.setProperty(
+            '--nav-height',
+            `${mainNav.value?.getBoundingClientRect().height ?? 48 + 1}px`
+        );
+    });
+});
 </script>
---
+++ components/layout/MobileNavigation.vue
@@ -0,0 +1,59 @@
+<template>
+    <Transition name="menu">
+        <nav v-if="menuIsOpen" class="menu">
+            <ul class="flex flex-col gap-8">
+                <li v-for="route in routes" class="">
+                    <Button
+                        :as-link="true"
+                        variant="secondary"
+                        active-class="text-white"
+                        class="underline-animation text-3xl text-gray"
+                        :to="route.to">
+                        <span> <span class="text-primary">#</span>{{ route.label }} </span>
+                    </Button>
+                </li>
+            </ul>
+            <ul class="mt-auto flex justify-center text-gray">
+                <li>
+                    <NuxtLink
+                        class="transition-colors duration-300 hover:text-primary"
+                        title="Take a look at my github"
+                        to="https://github.com/Mokerstier">
+                        <SvgComponent class="h-16 w-16" icon="Github"></SvgComponent>
+                    </NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink
+                        class="transition-colors duration-300 hover:text-primary"
+                        title="Take a look at my linkedin"
+                        to="https://nl.linkedin.com/in/wouter-van-der-heijde-610695175">
+                        <SvgComponent class="h-16 w-16" icon="Linkedin"></SvgComponent>
+                    </NuxtLink>
+                </li>
+            </ul>
+        </nav>
+    </Transition>
+</template>
+<script setup lang="ts">
+const routes = useNavigation();
+
+const props = defineProps<{
+    menuIsOpen: boolean;
+}>();
+</script>
+<style scoped lang="scss">
+.menu {
+    @apply absolute left-0 right-0 top-[calc(100%+1px)] z-10 flex h-[calc(100vh-var(--nav-height))] min-h-full w-full flex-col bg-slate px-4 pb-8 pt-12;
+}
+
+.menu-enter-active,
+.menu-leave-active {
+    transition: all 0.4s;
+}
+.menu-enter-from,
+.menu-leave-to {
+    opacity: 0;
+    filter: blur(1rem);
+    transform: translateY(-100%);
+}
+</style>
