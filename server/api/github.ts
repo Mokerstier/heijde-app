97c03bf7cc26a5fa4dcacec69995e983a0efc5aa
Wouter <wouter.van.der.heijde@outlook.com>
2024-12-16 22:25:17 +0100

    update github endpoint

--- server/api/github.ts
+++ server/api/github.ts
@@ -1,5 +1,13 @@
 import { Octokit } from 'octokit';
 
+export type RepoOutline = {
+    name: string;
+    description: string | null;
+    language: string | null;
+    updated_at: string;
+    homepage: string | null;
+};
+
 export default defineEventHandler(async (event) => {
     const { GITHUB_TOKEN } = useRuntimeConfig();
 
@@ -14,8 +22,22 @@ export default defineEventHandler(async (event) => {
     });
 
     const filteredReposOwnedByMe = repos.data.filter((repo) => repo.owner.login === 'Mokerstier');
-    const sortedData = filteredReposOwnedByMe.sort((a, b) => new Date(b.updated_at as string).getTime() - new Date(a.updated_at as string).getTime());
+    const sortedData = filteredReposOwnedByMe.sort(
+        (a, b) =>
+            new Date(b.updated_at as string).getTime() - new Date(a.updated_at as string).getTime()
+    );
     const twelveMostRecentProjects = sortedData.slice(0, 12);
+    const repoOutlines = twelveMostRecentProjects.map((repo) => transformRepoOutline(repo));
+    return repoOutlines as RepoOutline[];
+});
 
-    return twelveMostRecentProjects;
-  });
+const transformRepoOutline = (repo: any): RepoOutline => {
+    return {
+        name: repo.name,
+        description: repo.description,
+        language: repo.language,
+        updated_at: repo.updated_at,
+        homepage: repo.homepage,
+    };
+};
+
