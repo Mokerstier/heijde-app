0489608383ef87c388d93954d94fa1b410228745
Wouter <wouter.van.der.heijde@outlook.com>
2024-12-16 22:26:53 +0100

    Update Footer component

--- components/layout/MainFooter.vue
+++ components/layout/MainFooter.vue
@@ -2,17 +2,17 @@
     <footer class="border-t border-t-gray">
         <section class="container grid lg:grid-cols-2">
             <div class="col-span-2 flex flex-wrap items-center gap-x-10 gap-y-4 lg:col-auto">
-                <NuxtLink class="flex items-center gap-x-4" to="/">
+                <NuxtLink class="flex items-center gap-x-2" to="/">
                     <NuxtImg class="h-4 w-4" src="W-logo-light.svg" /> Wouter
                 </NuxtLink>
                 <a
                     href="mailto:wouter.van.der.heijde@outlook.com"
-                    class="text-gray hover:text-primary"
-                    >wouter.van.der.heijde@outlook.com</a
+                    class="w-full text-gray hover:text-primary md:max-w-max"
+                    >Mail me!</a
                 >
                 <span class="block w-full">Father && creative front-end developer</span>
             </div>
-            <div class="w-max-w lg-mt-0 ml-auto mt-8">
+            <div class="w-max-w lg-mt-0 mt-8 lg:ml-auto">
                 <h3 class="text-xl">Socials</h3>
                 <ul class="text-gray">
                     <li>
@@ -34,7 +34,7 @@
                 </ul>
             </div>
             <span class="col-span-2 mx-auto mt-12 inline-block text-gray">
-                © Copyright 2022. Made by Wouter
+                © Copyright 2024. Made by Wouter
             </span>
         </section>
     </footer>
