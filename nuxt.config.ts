324caaa21a1d1394576a2996d6047e19f05ad303
Wouter <wouter.van.der.heijde@outlook.com>
2024-12-16 22:23:05 +0100

    Added icons + pwa-assets-generator

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,23 +5,23 @@ export default defineNuxtConfig({
     app: {
         baseURL: '/heijde-app/',
         buildAssetsDir: 'assets',
-        pageTransition: { name: 'page', mode: 'out-in' }
-    } ,
-	compatibilityDate: "2024-04-03",
-	components: [
+        pageTransition: { name: 'page', mode: 'out-in' },
+    },
+    compatibilityDate: '2024-04-03',
+    components: [
         {
             path: '@/components',
             pathPrefix: false,
         },
     ],
     runtimeConfig: {
-        GITHUB_TOKEN: process.env.NUXT_GITHUB_TOKEN
+        GITHUB_TOKEN: process.env.NUXT_GITHUB_TOKEN,
     },
     ssr: true,
-	css: ['@/assets/scss/main.scss'],
-	devtools: { enabled: true },
-	modules: ["nuxt-mongoose", "@nuxtjs/tailwindcss", "@nuxt/image"],
-	vite: {
+    css: ['@/assets/scss/main.scss'],
+    devtools: { enabled: true },
+    modules: ['nuxt-mongoose', '@nuxtjs/tailwindcss', '@nuxt/image', '@vite-pwa/nuxt'],
+    vite: {
         css: {
             preprocessorOptions: {
                 scss: {
@@ -30,5 +30,34 @@ export default defineNuxtConfig({
             },
         },
         plugins: [svgLoader()],
+    },
+    pwa: {
+        manifest: {
+            display: 'standalone',
+            start_url: '/heijde-app/',
+            icons: [
+                {
+                    src: 'pwa-64x64.png',
+                    sizes: '64x64',
+                    type: 'image/png',
+                },
+                {
+                    src: 'pwa-192x192.png',
+                    sizes: '192x192',
+                    type: 'image/png',
+                },
+                {
+                    src: 'pwa-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                },
+                {
+                    src: 'maskable-icon-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                    purpose: 'maskable',
+                },
+            ],
+        },
     },
 });
